09a71a63dc96311766bec2910a51c71c58c4d14e
mirijass <miriamlozano204@gmail.com>
2024-06-26 20:06:53 -0600

    se agregaron las plantillas de las pantallas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegistroComponent } from './components/auth/registro/registro.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { MapaComponent } from './components/mapa/mapa.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistroComponent,
+    InicioComponent,
+    MapaComponent
   ],
   imports: [
     BrowserModule,
