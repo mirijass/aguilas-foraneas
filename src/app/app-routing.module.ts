dcd589f5e0d51b8c2f6d658e5a2775b834ed67bc
mirijass <miriamlozano204@gmail.com>
2024-06-26 20:23:41 -0600

    Se agregaron las rutas de los componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/auth/login/login.component';
+import { RegistroComponent } from './components/auth/registro/registro.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { MapaComponent } from './components/mapa/mapa.component';
 
 const routes: Routes = [
   {
     path: '',
+    component: LoginComponent,
+  },
+  {
+    path: 'login',
     component: LoginComponent
-  }
+  },
+  {
+    path: 'registro',
+    component: RegistroComponent
+  },
+  {
+    path: 'inicio',
+    component: InicioComponent
+  },
+  {
+    path: 'mapa',
+    component: MapaComponent
+  },
 ];
 
 @NgModule({
