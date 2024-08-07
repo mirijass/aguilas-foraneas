29ceabf2121b759d9fdcd7eda112d01c8dd3ba02
daniela <karladiazgonzalez488@gmail.com>
2024-08-07 10:32:42 -0600

    Modelo de lugares

---
+++ src/app/models/lugar.ts
@@ -0,0 +1,14 @@
+export interface Lugares {
+   id_lugar: string;
+   nombre_lugar: string;
+   latitud:string;
+   longitud: string;
+   descripcion: string;
+   calle: string;
+   colonia: string;
+   codigo_postal: string;
+   no_exterior: string;
+   no_interior: string;
+   telefono: string;
+   id_categoria: string;
+}
